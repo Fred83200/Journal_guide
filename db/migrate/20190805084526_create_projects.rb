05862fbbd40dd1bc80a854363c222cdc8437fc6d
frederic jonquieres <fjonquieres83@gmail.com>
2019-08-05 10:00:23 +0100

    added photo to projects

--- db/migrate/20190805084526_create_projects.rb
+++ db/migrate/20190805084526_create_projects.rb
@@ -8,6 +8,7 @@ class CreateProjects < ActiveRecord::Migration[5.2]
       t.string :database
       t.string :technology
       t.string :framework
+      t.string :photo
 
       t.timestamps
     end

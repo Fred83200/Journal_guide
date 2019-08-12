506aa5fbcdd690d2bf995cf4c74b972a29fb2b4e
frederic jonquieres <fjonquieres83@gmail.com>
2019-08-12 14:53:44 +0100

    took of the small on the tags, changed it for the index and show page so now the grid work again, added a few languages in the seed

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 2019_08_12_093256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "filters", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "pg_search_documents", force: :cascade do |t|
     t.text "content"
     t.string "searchable_type"
